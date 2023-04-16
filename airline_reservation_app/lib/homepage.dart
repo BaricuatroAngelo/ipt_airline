62772eb7c7d25c16ec27a49e4f516dd565adb97f
Angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2023-04-16 17:37:40 +0800

    homepage design

--- airline_reservation_app/lib/homepage.dart
+++ airline_reservation_app/lib/homepage.dart
@@ -6,18 +6,36 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Column(
-        children: const [
-         Padding(
-            padding: EdgeInsets.only(left: 30.0, top: 40.0),
-            child: Text(
-              'Book Your\nNext Flight',
-              style: TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 35,
-                color: Colors.teal,
-                wordSpacing: 5.0,
+        children: [
+          Positioned(
+            child: Container(
+              height: 244,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.blue, Colors.greenAccent]),
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50.0),
+                    bottomRight: Radius.circular(50.0)),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.only(left: 20, top: 45),
+                child: Text(
+                  'Book Your\nNext Flight',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                    wordSpacing: 5,
+                  ),
+                ),
               ),
             ),
           ),
