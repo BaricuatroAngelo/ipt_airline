41b422567c07647c90e5771776862d87f2b5c161
Angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2023-04-16 17:05:54 +0800

    text style

--- airline_reservation_app/lib/homepage.dart
+++ airline_reservation_app/lib/homepage.dart
@@ -1,18 +1,23 @@
 import 'package:airline_reservation_app/main.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [
-          Text('Book Your Next Flight'),
+        children: const [
+          Text(
+            'Book Your\nNext Flight',
+            style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 50,
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
