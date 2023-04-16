e08d9af41c51deb427bb6633ba0239c569926b21
Angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2023-04-16 16:18:59 +0800

    login page

---
+++ airline_reservation_app/lib/loginpage.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text(
+            "LOGIN",
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.w900,
+              color: Colors.teal,
+            ),
+          ),
+          const Text(
+            "Airline Reservation System",
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.w900,
+              color: Colors.teal,
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 1),
+            child: TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                labelText: "Enter Email",
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 1),
+            child: TextFormField(
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Enter Password",
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.lock),
+                suffixIcon: Icon(Icons.remove_red_eye),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Forget Password?",
+                    )),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Container(
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                gradient:
+                    const LinearGradient(colors: [Colors.blue, Colors.green]),
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                child: const Text(
+                  "LOGIN",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          const Divider(
+            height: 30,
+            color: Colors.black,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Don't have an account? ",
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.7),
+                ),
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Register Account",
+                  ))
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
