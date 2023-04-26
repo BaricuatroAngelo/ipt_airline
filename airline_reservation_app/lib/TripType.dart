6de43017b25c00b57325f2e1814e14e08005e485
Angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2023-04-26 14:33:47 +0800

    select one option only design implemented

--- airline_reservation_app/lib/TripType.dart
+++ airline_reservation_app/lib/TripType.dart
@@ -1,17 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TripType extends StatelessWidget{
-  const TripType({super.key, required this.title});
+class TripType extends StatefulWidget {
   final String title;
+  final Function(String)? onSelected;
+  final String? selectedValue;
+
+  const TripType({
+    Key? key,
+    required this.title,
+    required this.selectedValue,
+    this.onSelected,
+  }) : super(key: key);
 
   @override
-  Widget build (BuildContext context){
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Chip(
-        label: Text(
-          title,
-          style: const TextStyle(color: Colors.black),
+  State<TripType> createState() => _TripTypeState();
+}
+
+class _TripTypeState extends State<TripType> {
+  bool get _isSelected => widget.selectedValue == widget.title;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (!_isSelected) {
+          widget.onSelected?.call(widget.title);
+        }
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: _isSelected ? Colors.blue : Colors.grey[300],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Text(
+            widget.title,
+            style: TextStyle(
+              color: _isSelected ? Colors.white : Colors.black,
+            ),
+          ),
         ),
       ),
     );
