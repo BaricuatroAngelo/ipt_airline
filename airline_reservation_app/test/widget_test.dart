7d08f38904e2e4b21b45d9a227f48fbf88dd2226
baricuatro_angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2023-05-03 18:46:31 +0800

    small change

--- airline_reservation_app/test/widget_test.dart
+++ airline_reservation_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:airline_reservation_app/Screens/main.dart';
+import 'package:airline_reservation_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
