bc88d5ef7c993b65182d85faad570011f4e37afe
baricuatro_angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2023-05-05 09:20:17 +0800

    added unique=True for username

--- registration_backend/registration/models.py
+++ registration_backend/registration/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    username = models.CharField(max_length=40)
+    username = models.CharField(max_length=40, unique=True)
     first_name = models.CharField(max_length=30, default='')
     last_name = models.CharField(max_length=30, default='')
     email = models.EmailField(max_length=40)
